354f04af7544d4a5e18b67c6c49925aea85e198b
JimmyLv <jimmy.jinglv@gmail.com>
2023-03-05 10:01:28 +0800

    fix: update the info

--- next.config.js
+++ next.config.js
@@ -1,6 +1,14 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  async rewrites() {
+    return [
+      {
+        source: '/info',
+        destination: 'https://jimmylv.feishu.cn/docx/PguGdbTTXo93bExb1docMruNnGd',
+      },
+    ]
+  },
 }
 
 module.exports = nextConfig
