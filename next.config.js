6982a96f9ada0d63b98603249a7d04334518b0f6
JimmyLv <jimmy.jinglv@gmail.com>
2023-03-30 12:56:13 +0800

    fix: add marketing links

--- next.config.js
+++ next.config.js
@@ -1,11 +1,22 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  async rewrites() {
+  async redirects() {
     return [
       {
         source: '/info',
         destination: 'https://jimmylv.feishu.cn/docx/PguGdbTTXo93bExb1docMruNnGd',
+        permanent: true,
+      },
+      {
+        source: '/fox',
+        destination: 'https://www.quickfox.com.cn/landingV5?af_channel=cHJvbW90ZXI&af_referrer_customer_id=MTk3NTcz',
+        permanent: true,
+      },
+      {
+        source: '/bus',
+        destination: 'https://universalbus.cn/r?s=pdszpKz2fj',
+        permanent: true,
       },
     ]
   },
