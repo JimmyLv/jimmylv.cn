95beeccac6e0ec29f912ad234bee8bdee59decaf
JimmyLv <jimmy.jinglv@gmail.com>
2023-08-31 22:55:26 +0800

    feat: add noto.so redirect

--- next.config.js
+++ next.config.js
@@ -4,30 +4,39 @@ const nextConfig = {
   async redirects() {
     return [
       {
-        source: '/info',
-        destination: 'https://jimmylv.feishu.cn/docx/PguGdbTTXo93bExb1docMruNnGd',
-        permanent: true,
-      },{
-        source: '/b',
-        destination: 'https://space.bilibili.com/37648256',
-        permanent: true,
-      },{
-        source: '/bilibili',
-        destination: 'https://space.bilibili.com/37648256',
+        source: "/n/:slug*",
+        destination: "https://jimmylv.noto.so/:slug*",
         permanent: true,
       },
       {
-        source: '/fox',
-        destination: 'https://www.quickfox.com.cn/landingV5?af_channel=cHJvbW90ZXI&af_referrer_customer_id=MTk3NTcz',
+        source: "/info",
+        destination:
+          "https://jimmylv.feishu.cn/docx/PguGdbTTXo93bExb1docMruNnGd",
         permanent: true,
       },
       {
-        source: '/bus',
-        destination: 'https://universalbus.cn/r?s=pdszpKz2fj',
+        source: "/b",
+        destination: "https://space.bilibili.com/37648256",
         permanent: true,
       },
-    ]
+      {
+        source: "/bilibili",
+        destination: "https://space.bilibili.com/37648256",
+        permanent: true,
+      },
+      {
+        source: "/fox",
+        destination:
+          "https://www.quickfox.com.cn/landingV5?af_channel=cHJvbW90ZXI&af_referrer_customer_id=MTk3NTcz",
+        permanent: true,
+      },
+      {
+        source: "/bus",
+        destination: "https://universalbus.cn/r?s=pdszpKz2fj",
+        permanent: true,
+      },
+    ];
   },
-}
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
